fe33e87b7969dd39a3272c133048ca4aefe04e18
Aleksei Androsov <aandrosov@yandex-team.ru>
2016-12-09 17:35:30 +0300

    export generateReactRoutes

--- modules/index.js
+++ modules/index.js
@@ -2,5 +2,6 @@ export * from 'react-router';
 
 export createTransitionManager from './createTransitionManager';
 export extendRoutes from './extendRoutes';
+export generateReactRoutes from './generateReactRoutes';
 
 export Router from './Router';
